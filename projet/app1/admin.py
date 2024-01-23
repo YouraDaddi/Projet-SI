0c1347d6dcbfab11d788678efaf7c19c789de7a6
Yousra Daddi-Addoun <136271083+YouraDaddi@users.noreply.github.com>
2024-01-23 13:18:28 +0100

    creation

--- projet/app1/admin.py
+++ projet/app1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Centre , Produit , Client , Fournisseur , Employe, Vente_Produit,stock_Produit
+
+admin.site.register(Centre)
+admin.site.register(Produit)
+admin.site.register(Client)
+admin.site.register(Fournisseur)
+admin.site.register(Employe)
+admin.site.register(Vente_Produit)
+admin.site.register(stock_Produit)
 
 # Register your models here.
